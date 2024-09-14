b0deb8e711011439a2f39442974d18d1823a29c5
adimbrugh <haisamismail71@gmail.com>
2024-09-14 16:33:38 +0400

    add future years file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+current_age = int(input('How old are you ?'))
+
+future_year = 2050
+current_year = 2023
+
+years_to_add = future_year - current_year
+age_in_2050 = current_age + years_to_add
+
+print(f'In 2050, you will be {age_in_2050} years old')
